20edff397433ce40e0601bdd334851260e0c21d0
NOSHIRTEDKEEPER <117466542+NOSHIRTEDKEEPER@users.noreply.github.com>
2023-02-13 14:05:31 -0500

    pre_0.1

--- classes/Main.py
+++ classes/Main.py
@@ -1,13 +1,8 @@
-
+from classes.Terminal import Terminal
 class Main:
     def __init__(self):
-        self.user = "user"
-        self.comp = "my_comp"
-        self.BASHsymb = "$"
-        self.pre_cmdl = str(self.user) + "@" + str(self.comp) + str(self.BASHsymb)
-        self.cmd = self.terminal_start()
 
-    def terminal_start(self):
-        cmd = input(str(self.pre_cmdl))
-        return cmd
+        Terminal()
+
+
 
---
+++ classes/Terminal.py
@@ -0,0 +1,10 @@
+class Terminal:
+    def __init__(self):
+        self.user = "user"
+        self.comp = "my_comp"
+        self.BASHsymb = "$"
+        self.pre_cmdl = self.user + "@" + self.comp + self.BASHsymb
+        cmd = input(self.pre_cmdl)
+        self.process_command(cmd)
+    def process_command(cmd,self):
+        print(cmd)
