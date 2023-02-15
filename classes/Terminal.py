474a27078ffe1d29bdd16c1cf96512d0455b7a32
NOSHIRTEDKEEPER <117466542+NOSHIRTEDKEEPER@users.noreply.github.com>
2023-02-15 14:05:42 -0500

    pre_0.1

--- classes/Terminal.py
+++ classes/Terminal.py
@@ -1,11 +1,9 @@
-from classes.YT import YoutubeDownloader
-
 from classes.File_system import *
 
 class Terminal:
     def __init__(self):
         self.folder = root
-        self.commands = ["ls", "pwd", "cd", "yt"]
+        self.commands = ["ls", "pwd", "cd", "sudo su", "exit"]
         self.user = "user"
         self.comp = "my_comp"
         self.BASHsymb = "$"
@@ -19,8 +17,7 @@ class Terminal:
         while index < len(self.commands):
             if self.commands[index] in str(self.cmd):
                 self.process_command()
-                self.cmd = input(self.pre_cmdl)
-                self.check_command()
+
 
             index = index + 1
 
@@ -33,7 +30,6 @@ class Terminal:
                 print(str(self.cmd) + " is not a valid command.")
                 self.cmd = input(self.pre_cmdl)
                 self.check_command()
-
 
     def process_command(self):
         if str(self.cmd) == "ls":
@@ -57,17 +53,22 @@ class Terminal:
             self.cmd = input(self.pre_cmdl)
             self.check_command()
 
-        elif str(self.cmd) == "yt":
-            video_url = input("What is the url?")
-            video_path = input("What is the Path?")
-            if video_path == "" or " ":
-                video_path = "Windows/Users/Public/Public Downloads"
-
-            YoutubeDownloader.download_video(video_url = video_url, video_path= video_path)
-
         elif "cd" in str(self.cmd):
             safe = self.folder
             self.folder = input("what folder was that?")
             if self.folder != safe.children:
                 self.folder = safe
 
+        elif str(self.cmd) == "sudo su":
+            self.user = "Root"
+            self.BASHsymb = "#"
+            self.pre_cmdl = self.user + "@" + self.comp + self.BASHsymb
+            self.cmd = input(self.pre_cmdl)
+            self.check_command()
+
+        elif str(self.cmd) == "exit":
+            self.user = "user"
+            self.BASHsymb = "$"
+            self.pre_cmdl = self.user + "@" + self.comp + self.BASHsymb
+            self.cmd = input(self.pre_cmdl)
+            self.check_command()
